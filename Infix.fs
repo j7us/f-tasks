e4c81fa3d9b409be960c0c4f29db87b6351d2ceb
Egor Pozdnov <pozdner98@gmail.com>
2026-06-03 00:41:40 +0300

    Carry 2

--- Infix.fs
+++ Infix.fs
@@ -1,7 +1,7 @@
 // 23.4.1
 let (.+.) x y = 
     let sumMed ((a,b,c), (q,w,e)) = a*20*12 + q*20*12 + b*12 + w*12 + c + e
-    let countFull allMed = (allMed/20/12, allMed%20/12, allMed%20%12)
+    let countFull allMed = (allMed / (20 * 12), (allMed % (20 * 12)) / 12, allMed % 12)
     countFull (sumMed (x,y))
 
 
@@ -9,7 +9,7 @@ let (.-.) x y =
     let minusMed ((a,b,c), (q,w,e)) = a*20*12 - q*20*12 + b*12 - w*12 + c - e
     let countFull = function
      | f when f < 0 -> (-1,-1,-1)
-     | f -> (f/20/12, f%20/12, f%20%12)
+     | f -> (f / (20 * 12), (f % (20 * 12)) / 12, f % 12)
     countFull (minusMed (x,y))
 
 // 23.4.2
