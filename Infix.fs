2aa2de00d21742a89d4cc42e347d386453ef32cd
Egor Pozdnov <pozdner98@gmail.com>
2026-06-04 00:28:01 +0300

    Carry 2

--- Infix.fs
+++ Infix.fs
@@ -7,24 +7,22 @@ let (.+.) x y =
 
 let (.-.) x y = 
     let minusMed ((a,b,c), (q,w,e)) = a*20*12 - q*20*12 + b*12 - w*12 + c - e
-    let countFull = function
-     | f when f < 0 -> (-1,-1,-1)
-     | f -> (f / (20 * 12), (f % (20 * 12)) / 12, f % 12)
+    let countFull f =  (f / (20 * 12), (f % (20 * 12)) / 12, f % 12)
     countFull (minusMed (x,y))
 
 // 23.4.2
-let (.+) x y = 
+let (.+) (x: float * float) (y: float * float) = 
     let sum ((a,b), (c,d)) = (a+c, b+d)
     sum (x,y)
 
-let (.-) x y = 
-    let minus (a,b) = (a * (-1), b* (-1))
+let (.-) (x: float * float) (y: float * float) = 
+    let minus (a,b) = (a * (-1.0), b* (-1.0))
     x .+ (minus y)
 
-let (.*) x y = 
+let (.*) (x: float * float) (y: float * float) = 
     let sum ((a,b), (c,d)) = (a*c - b*d, b*c + a*d)
     sum (x,y)
 
-let (./) x y = 
+let (./) (x: float * float) (y: float * float) = 
     let del (a,b) = (a / (a * a + b * b), -b / (a * a + b * b))
     x .* (del y)
